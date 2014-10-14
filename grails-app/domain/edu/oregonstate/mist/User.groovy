7d036d6ad1417410e2022209fa81392d2b844faa
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-10-14 15:32:25 -0700

    define user fields and methods

--- grails-app/domain/edu/oregonstate/mist/User.groovy
+++ grails-app/domain/edu/oregonstate/mist/User.groovy
@@ -1,7 +1,44 @@
-package edu.oregonstate.mist
+package edu.oregonstate.mist;
 
 class User {
 
     static constraints = {
+    }
+
+    static hasMany = [tasks = Task];
+
+    protected String userName;
+    protected String emailAddress;
+    protected String passwordHash;
+
+    public User(String name, String email, String password) {
+        userName = name;
+        emailAddress = email;
+        passwordHash = hash(password);
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return emailAddress;
+    }
+
+    public void setEmail(String email) {
+        emailAddress = email;
+    }
+
+    public void setPassword(String password) {
+        passwordHash = hash(password);
+    }
+
+    public Boolean passwordEquals(String password) {
+        return (hash(password) == passwordHash);
+    }
+
+    /* TODO: use a real hash function */
+    private String hash(String clearText) {
+        return clearText;
     }
 }
