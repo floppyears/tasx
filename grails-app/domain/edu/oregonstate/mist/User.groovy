48fa2b0b9bdb632bafe1b34e695a0126fd5ab1e1
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-10-16 13:48:28 -0700

    omit optional semicolons

--- grails-app/domain/edu/oregonstate/mist/User.groovy
+++ grails-app/domain/edu/oregonstate/mist/User.groovy
@@ -1,39 +1,39 @@
-package edu.oregonstate.mist;
+package edu.oregonstate.mist
 
-import java.security.MessageDigest;
+import java.security.MessageDigest
 
 class User {
 
     static constraints = {
     }
 
-    static hasMany = [tasks: Task];
+    static hasMany = [tasks: Task]
 
-    private String userName;
-    String email;
-    private String passwordHash;
+    private String userName
+    String email
+    private String passwordHash
 
     public User(String name, String email, String password) {
-        userName = name;
-        this.email = email;
-        passwordHash = hash(password);
+        userName = name
+        this.email = email
+        passwordHash = hash(password)
     }
 
     public String getName() {
-        return userName;
+        return userName
     }
 
     public void setPassword(String password) {
-        passwordHash = hash(password);
+        passwordHash = hash(password)
     }
 
     public Boolean passwordEquals(String password) {
-        return (hash(password) == passwordHash);
+        return (hash(password) == passwordHash)
     }
 
     private static String hash(String clearText) {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(clearText.getBytes());
-        return new BigInteger(1, md.digest()).toString(16); // convert byte[] to BigInteger to hex String
+        MessageDigest md = MessageDigest.getInstance("SHA-256")
+        md.update(clearText.getBytes())
+        return new BigInteger(1, md.digest()).toString(16) // convert byte[] to BigInteger to hex String
     }
 }
