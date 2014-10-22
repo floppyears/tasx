f1a258c18ab98d17685cbcf826b36ba75243bce2
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-10-22 13:13:41 -0700

    unit test Interval.contains

--- test/unit/edu/oregonstate/mist/IntervalSpec.groovy
+++ test/unit/edu/oregonstate/mist/IntervalSpec.groovy
@@ -9,12 +9,56 @@ import spock.lang.Specification
 @TestFor(Interval)
 class IntervalSpec extends Specification {
 
+    Date before = new Date(),
+         from   = before + 1,
+         at     = before + 2,
+         to     = before + 3,
+         after  = before + 4
+
     def setup() {
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "interval contains date"() {
+        given:
+        Interval interval
+
+        when: "interval does not contain null"
+            interval = new Interval()
+        then:
+            interval.contains(null) == false
+
+        when: "empty interval contains instant"
+            interval = new Interval(at, at)
+        then:
+            interval.contains(before) == false
+            interval.contains(at)     == true
+            interval.contains(after)  == false
+
+        when: "open interval contains instant"
+            interval = new Interval(null, to)
+        then:
+            interval.contains(before) == true
+            interval.contains(to)     == true
+            interval.contains(after)  == false
+
+        when: "open interval contains instant"
+            interval = new Interval(from, null)
+        then:
+            interval.contains(before) == false
+            interval.contains(from)   == true
+            interval.contains(after)  == true
+
+        when: "closed interval contains instant"
+            interval = new Interval(from, to)
+        then:
+            interval.contains(before) == false
+            interval.contains(from)   == true
+            interval.contains(at)     == true
+            interval.contains(to)     == true
+            interval.contains(after)  == false
+    }
     }
 }
