8d8ca11e9a1c23b577441cf46041e31ce251c827
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-11-03 14:32:32 -0800

    define user register action
    tasx-7

--- grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
+++ grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
@@ -2,5 +2,17 @@ package edu.oregonstate.mist.tasx
 
 class UserController {
 
-    def index() { }
+    static defaultAction = ""
+
+    Map register() {
+        User newUser = new User((String)params.name, (String)params.email)
+
+        newUser.setPassword((String)params.pass1, (String)params.pass2)
+
+        if (newUser.validate()) {
+            redirect(action: "account")
+        }
+
+        return [ user: newUser ]
+    }
 }
