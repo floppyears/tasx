01c87ce93a7a1e67a9570d55c0e3e8f35c36b15c
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-11-04 13:58:25 -0800

    remove explicit casts
    tasx-7

--- grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
+++ grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
@@ -5,9 +5,9 @@ class UserController {
     static defaultAction = ""
 
     Map register() {
-        User newUser = new User((String)params.name, (String)params.email)
+        User newUser = new User(params.name, params.email)
 
-        newUser.setPassword((String)params.pass1, (String)params.pass2)
+        newUser.setPassword(params.pass1, params.pass2)
 
         if (newUser.validate()) {
             redirect(action: "account")
