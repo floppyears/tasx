8533eee8656457422f8f0e2f8e7abe9625770845
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-11-04 14:19:49 -0800

    use map constructor in user controller
    tasx-7

--- grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
+++ grails-app/controllers/edu/oregonstate/mist/tasx/UserController.groovy
@@ -5,7 +5,7 @@ class UserController {
     static defaultAction = ""
 
     Map register() {
-        User newUser = new User(params.name, params.email)
+        User newUser = new User([name: params.name, email: params.email])
 
         newUser.setPassword(params.pass1, params.pass2)
 
