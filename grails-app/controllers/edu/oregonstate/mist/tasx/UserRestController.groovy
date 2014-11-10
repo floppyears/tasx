eafdf8600a66cb1ff0c2570c0036817bba403cf4
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-11-10 13:24:23 -0800

    implement user rest index and show actions
    tasx-20

--- grails-app/controllers/edu/oregonstate/mist/tasx/UserRestController.groovy
+++ grails-app/controllers/edu/oregonstate/mist/tasx/UserRestController.groovy
@@ -2,5 +2,36 @@ package edu.oregonstate.mist.tasx
 
 class UserRestController {
 
-    def index() { }
+    static responseFormats = ["json"]
+
+    /**
+     * Respond with all users serialized as JSON
+     *
+     * $ curl -X GET -H "Accept:application/json" /tasx/rest/user
+     *
+     * @return       JSON response
+     */
+    def index() {
+        respond(User.findAll().collect(serializeUser))
+    }
+
+    /**
+     * Respond with user serialized as JSON
+     *
+     * $ curl -X GET -H "Accept:application/json" /tasx/rest/user/ID
+     *
+     * @param user   from URL
+     * @return       JSON response
+     */
+    def show(User user) {
+        respond(serializeUser(user))
+    }
+
+    private Closure serializeUser = {
+        user ->
+            [ id:    user.id,
+              name:  user.name,
+              email: user.email,
+              tasks: user.tasks ]
+    }
 }
