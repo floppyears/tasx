0885fe3029cf1201f6fe24944bfa8054c2882784
Taylor Brown <browtayl@onid.oregonstate.edu>
2014-12-11 15:32:50 -0800

    refactor navigation taglib
    tasx-21

--- grails-app/taglib/edu/oregonstate/mist/tasx/NavigateTagLib.groovy
+++ grails-app/taglib/edu/oregonstate/mist/tasx/NavigateTagLib.groovy
@@ -6,6 +6,17 @@ class NavigateTagLib {
 
     static final namespace = "tasx"
 
+    /**
+     * Generate navigation links depending on whether user is authenticated.
+     *
+     * Usage:
+     * <tasx:navigate authenticated="${(Boolean)session["user"]}" />
+     *
+     * @param attributes    [[authenticated: trueOrFalse]]
+     * @param body          empty tag body
+     *
+     * @return              formatted list of navigation links
+     */
     def navigate = {
         Map attributes, Closure body ->
             out << "<nav>"
@@ -15,21 +26,26 @@ class NavigateTagLib {
                          message: "tasx.index.title" ])
 
             if (attributes.authenticated) {
-                [[ url:     "/tasx/user",
-                   message: "tasx.user.account.title" ],
-                 [ url:     "/tasx/user/logout",
-                   message: "tasx.user.logout.title" ],
-                 [ url:     "/tasx/task/list",
-                   message: "tasx.task.list.title" ],
-                 [ url:     "/tasx/task/details",
-                   message: "tasx.task.list.new" ]].collect(outputLink)
+                outputLinks([[ url:     "/tasx/user",
+                               message: "tasx.user.account.title" ],
+                             [ url:     "/tasx/user/logout",
+                               message: "tasx.user.logout.title" ],
+                             [ url:     "/tasx/task/list",
+                               message: "tasx.task.list.title" ],
+                             [ url:     "/tasx/task/details",
+                               message: "tasx.task.list.new" ]])
             } else {
-                [[ url:     "/tasx/user/login",
-                   message: "tasx.user.login.title" ],
-                 [ url:     "/tasx/user/register",
-                   message: "tasx.user.register.title" ]].collect(outputLink)
+                outputLinks([[ url:     "/tasx/user/login",
+                               message: "tasx.user.login.title" ],
+                             [ url:     "/tasx/user/register",
+                               message: "tasx.user.register.title" ]])
             }
+
             out << "</nav>"
+    }
+
+    private outputLinks(List urlsAndMessages) {
+        urlsAndMessages.collect(outputLink)
     }
 
     private Closure outputLink = {
